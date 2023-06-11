410d5540c7c333131fc8564d85614c5519509ae0
Lorraine Cristina <cristinalorraine424@gmail.com>
2023-06-10 22:03:57 -0300

    Initial Commit

---
+++ CadastroApp/CadastroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadastroAppApp.swift
+//  CadastroApp
+//
+//  Created by lorraine cristina on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CadastroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
